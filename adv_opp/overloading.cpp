bbab391eb83fc8943f50c89bb70fc4c861f820c5
Ashutosh <ashutoshsinghdce@gmail.com>
2020-05-21 21:18:15 -0500

    merged class and function overload

--- adv_opp/overloading.cpp
+++ adv_opp/overloading.cpp
@@ -5,7 +5,9 @@ class Complex{
       private:
       int real, imag;
       public:
-      Complex(int i= 0 , int j= 0 ): real(i), imag(j){};
+      Complex(int i , int j ): real(i), imag(j){};
+      Complex(int i): real(i){imag = 44;};
+      Complex(){real = 0; imag = 0;};
       Complex operator + (Complex const &obj){
 	      Complex a2;
 	      a2.real = real + obj.real;
@@ -20,8 +22,8 @@ class Complex2{
       private:
       int real, imag;
       public:
-      Complex2(int i , int j ): real(i), imag(j){}
-      Complex2(int i):real(i){imag = 666;}
+      Complex2(int i , int j ) {real = i; imag = j;}
+      Complex2(){real = 99;imag = 666;}
       void printcomp2(){std::cout<<"real is "<<real<<", image is "<<imag<<std::endl;}
 
 };
@@ -30,12 +32,12 @@ class Complex2{
 
 
 int main(){
-	Complex c1(2,3),c2(4,6);
+	Complex c1(2,3),c2(4);
        	Complex c3 = c1+c2;
 	c3.printcomp();
-	Complex2 c4 = (9,9);
+	Complex2 c4(9,9);
 	c4.printcomp2();
-	Complex2 c5 = (9);
+	Complex2 c5;
 	c5.printcomp2();
 
 	return 0;
