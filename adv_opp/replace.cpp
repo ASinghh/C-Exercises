7389a8c38d059bfcfc545d6ca6a1f9e3f7202f6e
Ashutosh <ashutoshsinghdce@gmail.com>
2020-11-02 20:22:46 -0600

    added first exp

--- adv_opp/replace.cpp
+++ adv_opp/replace.cpp
@@ -1,5 +1,12 @@
 #include <string>
+using namespace std;
 
 void main(){
+
+    string line = "Name = Ubuntu";
+    std::replace(line.begin(), line.end(), ' ', '_');
+    std::replace(line.begin(), line.end(), '=', ' ');
+    std::replace(line.begin(), line.end(), '"', ' ');
+    cout<<line;
 
 }
