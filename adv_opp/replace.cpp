041661653d67ffdf5f3afec34dc3b247abc25b65
Ashutosh <ashutoshsinghdce@gmail.com>
2020-11-02 20:43:01 -0600

    new a.out

--- adv_opp/replace.cpp
+++ adv_opp/replace.cpp
@@ -1,18 +1,19 @@
-
 #include <string>
 #include <iostream>
+#include <algorithm> 
 
 using namespace std;
-using std::string;
-using std::to_string;
+
+
+
 
 
 int main(){
 
     string line = "Name = Ubuntu";
-    line.replace(line.begin(), line.end(), ' ', '_');
-    line.replace(line.begin(), line.end(), '=', ' ');
-    line.replace(line.begin(), line.end(), '"', ' ');
+    std::replace(line.begin(), line.end(), ' ', '_');
+    std::replace(line.begin(), line.end(), '=', ' ');
+    std::replace(line.begin(), line.end(), '"', ' ');
     std::cout<<line;
     return 0;
 
