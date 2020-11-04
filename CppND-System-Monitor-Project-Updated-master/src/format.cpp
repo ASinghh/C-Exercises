0efc69def2133f660dc36d410e307bcc86c43bc1
Ashutosh <ashutoshsinghdce@gmail.com>
2020-11-04 17:47:45 -0600

    removed day from ElapsedTime

--- CppND-System-Monitor-Project-Updated-master/src/format.cpp
+++ CppND-System-Monitor-Project-Updated-master/src/format.cpp
@@ -11,10 +11,8 @@ using std::to_string;
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
 string Format::ElapsedTime(long seconds) { 
-	int dd, hh, remaining_s, mm, ss;
+	int hh, remaining_s, mm, ss;
 
-	// Get days
-	dd = seconds / 86400;
 	remaining_s = seconds % 86400;
 	// Get hours
 	hh = remaining_s / 3600;
@@ -24,11 +22,7 @@ string Format::ElapsedTime(long seconds) {
 	// Get total remaining seconds
 	ss = remaining_s % 60;
 
-	if (dd == 0) {
-		return to_string(hh) + ":" + to_string(mm) + ":" + to_string(ss);
-	}
-	else{
-		return to_string(dd) + " days " + to_string(hh) + ":" + to_string(mm) + ":" + to_string(ss);
-	}
+	return to_string(hh) + ":" + to_string(mm) + ":" + to_string(ss);
+
 	
 }
