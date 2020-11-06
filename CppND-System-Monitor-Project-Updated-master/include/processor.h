9baae9fb9ed84add6a17b5a63a1e71626df7c29e
Ashutosh <ashutoshsinghdce@gmail.com>
2020-11-05 20:07:49 -0600

    added Processor utilization in processor.cpp

--- CppND-System-Monitor-Project-Updated-master/include/processor.h
+++ CppND-System-Monitor-Project-Updated-master/include/processor.h
@@ -6,7 +6,13 @@ class Processor {
   float Utilization();  // TODO: See src/processor.cpp
 
   // TODO: Declare any necessary private members
+ 
  private:
+  long total_jiffies_start = 0;
+  long active_jiffies_start = 0;
+  long total_jiffies_end = 0;
+  long active_jiffies_end = 0;	
+
 };
 
 #endif
